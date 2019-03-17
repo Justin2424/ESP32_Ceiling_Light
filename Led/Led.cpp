e0e78259b314e033094cab0b1ab0290b48e6efc9
Justin2424 <jlm0924@yahoo.com>
2019-03-17 16:45:18 -0400

    Update Led.cpp

--- Led/Led.cpp
+++ Led/Led.cpp
@@ -10,20 +10,21 @@
 #include "Led.h"
 #include <EEPROM.h>
 
-#define EEPROM_SIZE 20			 			
+#define EEPROM_SIZE 20	
+#define START 0		 																							
+#define DISABLE 99999999
 
 Led* Led::instances[MAX_LED_COUNT] ={};													// keep a list of the leds
 size_t Led::instanceCount = 0;																	// # of leds in list
 uint32_t Led::_last_update_time = 0;														// update only every 5ms or so
 
-
-uint32_t Led::_alarmCounter = 9999999;													// reset Counter to zero to activate this mode
-uint32_t Led::_notifyCounter = 9999999;													// reset Counter to zero to activate this mode
-uint32_t Led::_blinkCounter = 9999999;													// reset Counter to zero to activate this mode
-uint32_t Led::_trackingCounter = 9999999;												// reset Counter to zero to activate this mode
+uint32_t Led::_alarmCounter = DISABLE;													// reset Counter to zero to activate this mode
+uint32_t Led::_notifyCounter = DISABLE;													// reset Counter to zero to activate this mode
+uint32_t Led::_sceneCounter = DISABLE;													// reset Counter to zero to activate this mode
+uint32_t Led::_trackingCounter = DISABLE;												// reset Counter to zero to activate this mode
 
 /* given a value 0-100, returns a value 0-255 */ 
-static int percentTo255(double value){
+static int scale255(double value){
 	// expodential scaling to preserve low value resolution.. and looks more linear when fading (with my leds atleast)
   return pow(value, 1.203271);  
 }
@@ -39,7 +40,7 @@ void Led::setup(void){
 		delay(5);
     instances[i]->_onState = EEPROM.read(10+i);
 		//Serial.print(" aftr read b "); Serial.print(instances[i]->_brightness); Serial.print("  after read s "); Serial.println(instances[i]->_onState);
-		if(instances[i]->_onState) ledcWrite(instances[i]->_channel, percentTo255(instances[i]->_brightness));
+		if(instances[i]->_onState) ledcWrite(instances[i]->_channel, scale255(instances[i]->_brightness));
 		
 	}
 }
@@ -55,6 +56,7 @@ Led::Led(uint8_t pin, uint8_t channel, char* sub_on, char* pub_on, char* sub_bri
     _brightness = 100;																	// 0-100 are valid values
     _onState = false;																		// led on / led off
 		_fadeCounter = 9999999;															// fade progress / trigger..// reset Counter to zero to activate this mode
+		_totalFadeSteps=1;																	// the total fadeCounter ticks required to complete the fade
 		_memoryAddress = instanceCount;											// eeprom address for saving ledState and brightness
 		ledcSetup(channel, FREQ, RESOLUTION);								// pwmsetup
   	ledcAttachPin(pin, channel);												// pwm setup
@@ -69,13 +71,11 @@ void Led::on(void){
 		_onState = true;																		// set the Led on
 		EEPROM.write(10+_memoryAddress, _onState);  				// save new onState
     EEPROM.commit();
-		_fadeCounter = 0;																		// activate fade
-		/*
-		_alarmCounter = 9999999;														// make sure all other modes are canceled
-		_notifyCounter = 9999999;
-		_blinkCounter = 9999999;
-		_trackingCounter = 9999999; 
-		*/
+		_fadeCounter = START;																// activate fade
+		_alarmCounter = DISABLE;														// make sure all other modes are canceled
+		_notifyCounter = DISABLE;
+		_sceneCounter = DISABLE;
+		_trackingCounter = DISABLE; 
 	}
 }
 
@@ -86,60 +86,57 @@ void Led::off(void){
 		_onState = false;																		// set the led state to off
 		EEPROM.write(10+_memoryAddress, _onState);  				// save new onState
     EEPROM.commit();
-	 	_fadeCounter = 0;																		// activate fade
-		 /*
-		_alarmCounter = 9999999;														// make sure other modes are canceled
-		_notifyCounter = 9999999;
-		_blinkCounter = 9999999;
-		_trackingCounter = 9999999; 
-		*/
+	 	_fadeCounter = START;																// activate fade
+		_alarmCounter = DISABLE;														// make sure other modes are canceled
+		_notifyCounter = DISABLE;
+		_sceneCounter = DISABLE;
+		_trackingCounter = DISABLE; 
+		
 	}
 }
 
 
 void Led::setBrightness(int newBrightness){					
-	if(newBrightness > 0){																	// don't allow zero brightness (or possible negitive values)
+	if(newBrightness != _brightness){																	// don't allow zero brightness (or possible negitive values)
 		if (_onState){																				// set fade to newbrightness 
 			_startFadeValue = _brightness;											// brightness level to start the fade
 			_endFadeValue = newBrightness;											// brightness level the fade will stop
-				_fadeCounter = 0;																	// activate fade
-			/*_alarmCounter = 9999999;													// cancel all other modes
-			_notifyCounter = 9999999;
-			_blinkCounter = 9999999;
-			_trackingCounter = 9999999; 
-			*/
+			_fadeCounter = START;																// activate fade
 		}
 		_brightness = newBrightness;													// set new brightness
 		EEPROM.write(_memoryAddress, newBrightness);  				// save new brightness
 		EEPROM.commit();
-	}
-	else off();						// if zero brightness is requested, keep the current brightness setting and fade to off	state		
+	}	
+	_alarmCounter = DISABLE;																// cancel all other modes
+	_notifyCounter = DISABLE;
+	_sceneCounter = DISABLE;
+	_trackingCounter = DISABLE; 		
 }
 
-bool Led::toggle(void){																	// toggle (off / last used brightness level)
-	if(_onState)off();
+bool Led::toggle(void){																		// toggle (off / last used brightness level)
+	if(this->_onState)off();
 	else on();
-	return _onState;
+	return this->_onState;
 }
-
 
 bool Led::isOn(){
-	return _onState;																			// return if led is on
+	return _onState;																				
 }
 
 void Led::alarm(void){
-	_alarmCounter = 0;																		//  activate
+	_alarmCounter = START;																	//  activate
 }
 void Led::notify(void){
-	_notifyCounter = 0;																		//  activate
+	_notifyCounter = START;																	//  activate
 	
 }
-void Led::blink(void){
-	_blinkCounter = 0;																		//  activate
+void Led::scene(void){
+	_sceneCounter = START;																	//  activate
 }
 void Led::tracking(void){
-	_trackingCounter = 0;																	//  activate
+	_trackingCounter = START;																//  activate
 }
+
 
 /*
  *	populates the partial COSIGNE_WAVE array with values (s-curve)
@@ -166,32 +163,42 @@ void Led::_setFadeCurve(int startValue, int endValue){ 		// Lets pre calculate a
 }
 */
 
-/*
- *	
+
+/* Given counter: returns a value between 0-1. The returned value ramps up to 1, then ramps down to 0, and continues			
+ * to osillate every multiple of 'toggleDirectionEvery'...                                         			
  */
-int Led::_getNextFadeValue(){ 		
-	// (could remove declarations from the loop for speed)
-	int delta = _endFadeValue - _startFadeValue;
-	_totalFadeSteps = pow(100 * abs(delta), 0.5);															// the # steps used to transition 
-  double position = cos(PI * _fadeCounter / _totalFadeSteps) ;					    // a position on the half cosine value, somewhere between 1 and -1 
-	int value = round((position  * -0.5 + 0.5) * delta);      								//  mapped to 0-1, then multiplied by delta and rounded
-	
-	return (int)_startFadeValue + value;
-		
+static float oscillate(long counter, float toggleDirectionEvery=100){
+	return cos(counter/toggleDirectionEvery * PI) * -0.5 + 0.5;     // the y value of a cosine wave remapped (from 1 to -1) to 0-1
 }
 
-void Led::_restoreLedState(int value){
-	Serial.println(" Test 1");
-	if(value > 0){     						// the led is on
-	Serial.println(" Test 2");
-		int temp = _brightness;
-		_brightness = value;
+/* This calculates the next brightness level in the fade.. This is activated in the loop when fadeCounter is reset to zero,
+ *	and continues to run until fadeCounter ==_totalFadeSteps.
+ */
+int Led::_getNextFadeValue(){ 		
+	int delta = _endFadeValue - _startFadeValue;
+		// we could just increment brightness till we reach the new target, but lets use cosine to remap those values
+		// to more natural transition (s-curve vs line)
+		_totalFadeSteps = 100;//pow(100 * abs(delta), 0.5);									// first, lets also remap the # steps used to transition, instead of using delta
+		// get our y position on the s-curve (0-1) 
+		if(delta!=0){
+			float progress = oscillate(_fadeCounter,_totalFadeSteps) ;		// fade counter will be stopped at _totalFadeSteps, so we do dont oscillate 
+			return (int)_startFadeValue + round(progress  * delta);				// return brightness level  
+		}	
+		else return _brightness;
+}
+
+/* The Notify, Alarm, Track and Scene modes (when triggered) may temporily manipulate the leds brightness 
+ * and state without overwriting the settings... this function can then be used to return the led to its 
+ * settings.. Given the leds current actual brightness.
+ */
+void Led::_restoreLedState(int fromBrightness){
+	if(fromBrightness > 0){     						// the led is on
+	  int temp = _brightness;
+		_brightness = fromBrightness;
 		if(_onState){ 							// resync: fade from value to _brightness
-		Serial.println(" Test 3");
-			setBrightness(temp);
+		  setBrightness(temp);
 		}
 		else{												// resync: fade from value to off
-				Serial.println(" Test 4");
 				_onState = true;
 				off();
 		} 	
@@ -207,33 +214,35 @@ void Led::_restoreLedState(int value){
 	}	
 }
 
+
+
+
+
 /*
  *  put this in the main loop
 */
 void Led::loop(void){
 
-	if (millis() - _last_update_time < _UPDATE_SPEED) return;						// only updated every 5ms or so
+	if (millis() - _last_update_time < _UPDATE_SPEED) return;						// only updated every 5ms 
 	_last_update_time = millis();
 
 	////////////////  FADE_MODE  ///////////////		 
 	for(int i=0; i < instanceCount; i++){
-
-		if(instances[i]->_fadeCounter <= instances[i]->_totalFadeSteps){	
-			ledcWrite(instances[i]->_channel, percentTo255(instances[i]->_getNextFadeValue()));
+ 		if(instances[i]->_fadeCounter <= instances[i]->_totalFadeSteps){	
+			ledcWrite(instances[i]->_channel, scale255(instances[i]->_getNextFadeValue()));
 			instances[i]->_fadeCounter++;
 		}
 	}
 
 	
 	///////// ALARM_MODE ////////
-	if(_alarmCounter <= 20000){ 			
-		for (int i=0; i< instanceCount; i++){          											// for each pin
-		  	int rollOverCounter = int(_alarmCounter + (12.5*i)) % 100;      // is a rolling over value between 0-100 // (12.5*i) is an offset for each pin/led
-		  	float sinVal =  (sin(rollOverCounter * 0.062831));  //(2PI/100) // a value between -1 and 1 // as _alarmCounter increments: SinVal starts at 0, ramps up to 1, and falls back to zero.. (for the first pin)// half sinewave, with no negitive values.
-		  	int value = (sinVal * 0.5 + 0.5 ) * 255;												// mapped to 0-1, then multiplied
-				ledcWrite(instances[i]->_channel, value);											  // turn led to value
+	if(_alarmCounter <= 2000){ 																				  	// length of effect
+		for (int i=0; i< instanceCount; i++){          											// loop for each led
+		  	int rollOverCounter = _alarmCounter % 100;   										// is a rolling over value between 0-100
+		  	float sinVal =  oscillate(rollOverCounter + (12.5*i), 50); 			// a value between 0 and 1 .. (12.5*i) is an offset for each led
+		  	ledcWrite(instances[i]->_channel, sinVal * scale255(100));			// turn led on to brightness value
 
-			  if (_alarmCounter == 20000) instances[i]->_restoreLedState(value);//finished, set all leds to fade to orginal brightness state
+			  if (_alarmCounter == 2000) instances[i]->_restoreLedState( sinVal*100 );//finished, set all leds to fade to orginal brightness state
 
 		}
 		_alarmCounter++;
@@ -241,35 +250,29 @@ void Led::loop(void){
 	
 
 	/////////  NOTIFY_MODE  ///////// 
-	else if(_notifyCounter <= 200){ 		//    a subtle variation of Alarm Mode
-		
-		for (int i=0; i < instanceCount; i++){                        	// for each led
-			int rollOverCounter = int(_notifyCounter + (12.5*i)) % 100;   // is a rolling over value between 0-100 // (12.5*i) is a different offset for each pin/led
-			float sinVal =  (sin(rollOverCounter*0.062831));        			//(2PI/100)    // a value between -1 and 1
-			int value = (sinVal * 0.5 + 0.5 ) * 120;											// mapped to 0-1, then multiplied
-			ledcWrite(instances[i]->_channel, value);											// turn led to value
+	else if(_notifyCounter <= 300){ 																// length of effect
+		float effectBrightness = oscillate(_notifyCounter,150)*100;		// fade effect in, fade effect out... (0-1-0) *100 // (0-1 the first 150 ticks, 1-0 the next 150 ticks)
+		float osc = oscillate(_notifyCounter,100)*100;								// 
+		for (int i=0; i < instanceCount; i++){                        // for each led
+			float ledPosition = oscillate(osc + (14.2857*i),100);    		// ((100/7*i) is a different offset for each pin/led
+			int value = ledPosition * effectBrightness; 								// turn led to value
+			//int value = abs(instances[i]->_brightness - int(ledPosition*effectBrightness) ); // not sure why this doesn't work as intended
+			ledcWrite(instances[i]->_channel, scale255(value));
 			
-			if (_notifyCounter == 200) instances[i]->_restoreLedState(value);//finished, set all leds to fade to orginal brightness state
+			if (_notifyCounter == 300) instances[i]->_restoreLedState(value); // finished, set all leds to fade back to their settings
 
 		}
 		_notifyCounter++;
 	}
 	
 
-	//////// BLINK_MODE ////////
-	else if(_blinkCounter <= 400){
-		int value = 255;
-		for (int i=0; i< instanceCount; i++){          			// for each pin
-			if (_blinkCounter < 100) value = 255;
-			else if (_blinkCounter < 200) value = 0;
-			else if (_blinkCounter < 300) value = 255;
-			else value = 0;
-			ledcWrite(instances[i]->_channel, value);
-			
-			if (_blinkCounter == 400) instances[i]->_restoreLedState(value);//finished, set all leds to fade to orginal brightness state
-
+	//////// SCENE_MODE ////////
+	else if(_sceneCounter == 0){
+		for (int i=0; i < instanceCount; i++){                        	// loop each led
+			float b = i * 14.2857;    																		// a different brightness 0-100 for each led   //  b=i*100/7 
+			ledcWrite(instances[i]->_channel, scale255(b));					
 		}
-		_blinkCounter++;
+		_sceneCounter = DISABLE;
 	}
 	
 
@@ -289,14 +292,3 @@ void Led::loop(void){
 	}*/
 
 }
-
-
-
-
-
-
-
-
-
-
-
